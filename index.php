90da7d66c28d1186f92c04333a5d9188404eea40
Marek Melichar <koprivackej@gmail.com>
2017-07-09 20:43:38 +0200

    remade to be a theme

--- index.php
+++ index.php
@@ -1,13 +1,5 @@
 <?php
 
-/*
-Plugin Name: APP USING WP REST API WITH REACT JS
-Version: 1.0.0
-Text Domain: wp-rest-api-with-react
-Author: Marek Melichar
-Author URI: marekmelichar.cz
-License:     GPL2
-License URI: https://www.gnu.org/licenses/gpl-2.0.html
-*/
+
 
 require_once 'functions.php';
